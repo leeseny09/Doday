f97933be5ccdc27cac1e56538eff698de3f88ba3
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-05-30 16:29:25 +0900

    feat: add SecurityConfig with filterChain

--- backend/src/main/java/com/doday/backend/global/auth/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/doday/backend/global/auth/JwtAuthenticationFilter.java
@@ -7,13 +7,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
-
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     // JwtUtil 가져오기 -> 필터에서 토큰 검증할 때 물어봐야하기에
--- backend/src/main/java/com/doday/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/doday/backend/global/config/SecurityConfig.java
@@ -1,16 +1,45 @@
 package com.doday.backend.global.config;
 
+import com.doday.backend.global.auth.JwtAuthenticationFilter;
+import com.doday.backend.global.auth.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration // 설정 클래스 어노테이션
 @EnableWebSecurity // 스프링 시큐리티 활성화
-
+@RequiredArgsConstructor
     public class SecurityConfig {
 
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
+        private final OAuth2SuccessHandler oAuth2SuccessHandler;
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http
+                    .csrf(csrf->csrf.disable()) //jwt 사용, 세션 사용 안함
+                    .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                    .authorizeHttpRequests(auth -> auth
+                            // 회원가입, 로그인은 누구나 접근 가능
+                            .requestMatchers("/api/auth/**").permitAll()
+                            .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                            .requestMatchers("/api/**").authenticated()
+                            .anyRequest().permitAll())
+                    // 구글 로그인 설정
+                    .oauth2Login(oauth2 ->oauth2
+                            .successHandler(oAuth2SuccessHandler))
+                    // jwt 필터를 시큐리티 앞에 추가
+
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
+        }
         @Bean // 스프링이 관리하는 객체로 등록
         public PasswordEncoder passwordEncoder() {
             return new BCryptPasswordEncoder();
