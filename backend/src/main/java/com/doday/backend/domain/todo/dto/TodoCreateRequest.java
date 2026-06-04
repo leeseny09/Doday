03bd7938f16fee9612bbe6eed034c60b31088cdb
seyeon09 <tpdus0919@dongyang.ac.kr>
2026-06-04 15:49:45 +0900

    feat: add TodoCreateRequest and TodoResponse DTO

---
+++ backend/src/main/java/com/doday/backend/domain/todo/dto/TodoCreateRequest.java
@@ -0,0 +1,21 @@
+package com.doday.backend.domain.todo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoCreateRequest {
+
+    @NotBlank
+    private String title; // 할일 제목 ( 필수 )
+
+    private String category; // 카테고리 ( 선택 )
+
+    private LocalDateTime deadlineTime; // 마감 시간 ( 선택 )
+}
